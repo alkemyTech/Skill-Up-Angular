fcc96c5beffc329b90decde0060d1440c03dbd4f
RaCode75 <racode75@gmail.com>
2022-11-02 18:50:44 -0300

    repo cloned

--- src/app/auth/registro/registro.component.ts
+++ src/app/auth/registro/registro.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthModule } from '../auth.module';
+
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  //variables
+  registerForm: FormGroup;
+  isSubmitted = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+    ) {
+    this.registerForm = this.fb.group({
+      fName:[ "", Validators.required ],
+      lName:[ "", Validators.required ],
+      email: [ "", Validators.required ],
+      password: [ "", Validators.required ],
+      
+
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+  register(){
+    this.isSubmitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
   }
 
 }
